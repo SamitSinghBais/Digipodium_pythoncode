4a146958a3bcfdd30e32d4728c38804ddb77eb89
samit <itssamitsingh@gmail.com>
2023-03-30 23:48:50 +0530

    latest_update

--- hello.py
+++ hello.py
@@ -3,13 +3,13 @@ speed('slowest')
 pencolor('green')
 fillcolor('blue')
 
-for i in range(6):
+for i in range(3):
    fd(200)
-   for i in range(6):
+   for i in range(3):
       fd(100)
       begin_fill()
-      for i in range(6):
-        fd(50)
+      for i in range(3):
+        fd(1)
         rt(60)
       end_fill()
       lt(60)
