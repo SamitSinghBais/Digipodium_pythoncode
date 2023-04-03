7f9e62763632c6389e56a69d0aba4ba524603391
samit <itssamitsingh@gmail.com>
2023-04-03 23:10:46 +0530

    main_pra

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+with open("iris.data") as f:
+    data = f.read()
+    data = data.split("\n")
+
+#print(data)
+newData = []
+for line in data:
+    #print(line)
+    newData.append(line.split(","))
+
+#print(newData)
+df = pd.DataFrame(newData, columns=["c1","c2","c3","c4","type"])
+df.to_csv("py.csv",index=False)
